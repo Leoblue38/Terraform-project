290f83023207ed91694d51596cbcac9daaafd333
Srinivasan S Chakravarthy <sinu.cosu@gmail.com>
2022-08-04 10:27:08 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "srini-web" {
     ami = "ami-068257025f72f470d"
-    instance_type = t2.micro
+    instance_type = "t2.micro"
     count = "1"
     subnet_id = "subnet-0d47c7cc17ec8f644"
 
